a2fbccb07f873f29ce0c0b85456d52b6994b8a15
Irell Zane <itsirellzane@gmail.com>
2024-05-19 20:05:06 +0100

    Dockerize flask app

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+    app.run()
