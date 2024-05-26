37b034af8219764801da6b656a09687ab55e3746
Irell Zane <itsirellzane@gmail.com>
2024-05-26 15:14:09 +0100

    Display company list on frontend

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify
 import psycopg2
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['DATABASE'] = {
     'host': 'db',
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import { BACKEND_URL } from './config/constants'; // Import the URL from the constants file
 
 function App() {
+  const [companies, setCompanies] = useState([]);
+
+  useEffect(() => {
+    axios.get(`${BACKEND_URL}companies`)
+      .then(response => {
+        console.log('Response:', response.data);
+        setCompanies(response.data.companies);
+      })
+      .catch(error => {
+        console.error('Error fetching companies:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Company List</h1>
       </header>
+      <main>
+        <ul>
+          {companies.map((company, index) => (
+            <li key={index}>{company}</li>
+          ))}
+        </ul>
+      </main>
     </div>
   );
 }
