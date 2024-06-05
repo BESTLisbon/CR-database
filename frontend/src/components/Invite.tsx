5f1f2fa35c7568e2dae0d88c75fed419134b4cd1
André Silva <andre.miguel.silva@tecnico.ulisboa.pt>
2024-06-05 21:07:14 +0100

    feat: Invite route on frontend (#11)

---
+++ frontend/src/components/Invite.tsx
@@ -0,0 +1,47 @@
+import React, { useState, FormEvent } from 'react';
+import { BACKEND_URL } from '../config/constants';
+import axios from 'axios';
+import { useAuth } from '../context/AuthContext';
+
+const Invite: React.FC = () => {
+  const [email, setEmail] = useState<string>('');
+  const [success, setSuccess] = useState<boolean>(false);
+
+  const invite = async (email: string): Promise<void> => {
+    try {
+      const response = await axios.post(`${BACKEND_URL}auth/invite`, {
+        email,
+      });
+
+      if (response.status === 200) {
+        setSuccess(true);
+      } else {
+        console.error(response.data.error);
+      }
+    } catch (error) {
+      console.error('An error occurred:', error);
+    }
+  };
+
+  const handleSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+    await invite(email);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Email:</label>
+        <input
+          type='text'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+      <button type='submit'>Invite</button>
+    </form>
+  );
+};
+
+export default Invite;
